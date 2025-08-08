f2a086ebfc83114d0c8e6ac8dace4761374f35b6
gianpaolo.papaiz <gianpaolo.papaiz@behaviorsoft.com>
2025-08-08 20:15:41 -0300

    initial commit

---
+++ BlazorHybridApp/BlazorHybridApp.Web/Services/FormFactor.cs
@@ -0,0 +1,16 @@
+using BlazorHybridApp.Shared.Services;
+
+namespace BlazorHybridApp.Web.Services;
+
+public class FormFactor : IFormFactor
+{
+    public string GetFormFactor()
+    {
+        return "Web";
+    }
+
+    public string GetPlatform()
+    {
+        return Environment.OSVersion.ToString();
+    }
+}
---
+++ BlazorHybridApp/BlazorHybridApp/Services/FormFactor.cs
@@ -0,0 +1,16 @@
+using BlazorHybridApp.Shared.Services;
+
+namespace BlazorHybridApp.Services;
+
+public class FormFactor : IFormFactor
+{
+    public string GetFormFactor()
+    {
+        return DeviceInfo.Idiom.ToString();
+    }
+
+    public string GetPlatform()
+    {
+        return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+    }
+}
